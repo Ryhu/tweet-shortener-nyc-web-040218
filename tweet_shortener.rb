e090b4d0dbb5706fbbc2d95d24d9333ab2669b2d
Ryan Hu <ryanhuuu@gmail.com>
2018-03-24 21:02:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,5 +4,11 @@ def word_substituter(tweet)
 end
 
 def dictionary()
-  
+  "hello" => 'hi',
+  "to, two, too" => '2' ,
+  "for, four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@" ,
+  "and" => "&"
 end
